08a9adf1bafd89ae0298098de2049426ca0962e5
simransindhwani <30287028+simransindhwani@users.noreply.github.com>
2020-07-15 23:03:26 -0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -17,6 +17,7 @@ namespace VS2019_CSharpGitDemo
              Console.WriteLine("Changes made on Github Again Final 23");
              Console.WriteLine("Changes made on Github Again Final 22");
              Console.WriteLine("CHECK");
+            Console.WriteLine("Works. YAS!");
             
         }
     }
